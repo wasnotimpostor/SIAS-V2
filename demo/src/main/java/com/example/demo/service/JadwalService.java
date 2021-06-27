63cc9f8afd42d781c5c87549d4f577fc2c79b8b3
wasnotimpostor <evan.mahendra95@gmail.com>
2021-06-28 00:15:30 +0700

    service jadwal

---
+++ demo/src/main/java/com/example/demo/service/JadwalService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.dtoJadwal;
+import com.example.demo.model.Jadwal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JadwalService {
+    Page<dtoJadwal> getJadwalByPage(Integer id, Integer idHari, Pageable pageable);
+    List<dtoJadwal> getJadwalByList();
+    Jadwal save(Jadwal jadwal);
+    Jadwal delete(Integer id);
+    Optional<Jadwal> findById(Integer id);
+}
