3660a1053257654c97b2b2a52cca62db73e855f1
wasnotimpostor <evan.mahendra95@gmail.com>
2021-06-27 23:05:10 +0700

    entity jadwal

---
+++ demo/src/main/java/com/example/demo/model/Jadwal.java
@@ -0,0 +1,41 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "Jadwal")
+public class Jadwal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "jam")
+    private Integer jam;
+
+    @Column(name = "matpel")
+    private Integer matpel;
+
+    @Column(name = "guruPengajar")
+    private Long guruPengajar;
+
+    @Column(name = "kelas")
+    private Integer kelas;
+
+    @JsonIgnore
+    @Column(name = "createdAt")
+    private Date createdAt;
+
+    @JsonIgnore
+    @Column(name = "updatedAt")
+    private Date updatedAt;
+}
