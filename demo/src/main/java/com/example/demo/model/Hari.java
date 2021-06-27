b8e669dc2a21abe4bcef43dbb4ccaf0abf4b87e2
wasnotimpostor <evan.mahendra95@gmail.com>
2021-06-28 00:08:00 +0700

    entity hari

---
+++ demo/src/main/java/com/example/demo/model/Hari.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "Hari")
+public class Hari {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonIgnore
+    @Column(name = "createdAt")
+    private Date createdAt;
+
+    @JsonIgnore
+    @Column(name = "updatedAt")
+    private Date updatedAt;
+}
