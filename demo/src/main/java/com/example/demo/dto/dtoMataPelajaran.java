04cc16ed0bd6c82cb9bf44931468c5a286f2a913
wasnotimpostor <evan.mahendra95@gmail.com>
2021-06-28 00:14:49 +0700

    dto mata pelajaran

---
+++ demo/src/main/java/com/example/demo/dto/dtoMataPelajaran.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class dtoMataPelajaran {
+
+    private Integer id;
+    private String name;
+    private String fullname;
+}
