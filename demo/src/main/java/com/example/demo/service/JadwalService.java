cd5e8bd097307673c3deabcc8a564237eccd8d19
wasnotimpostor <evan.mahendra95@gmail.com>
2021-06-28 23:11:47 +0700

    service jadwal

--- demo/src/main/java/com/example/demo/service/JadwalService.java
+++ demo/src/main/java/com/example/demo/service/JadwalService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface JadwalService {
-    Page<dtoJadwal> getJadwalByPage(Integer id, Integer idHari, Pageable pageable);
+    Page<dtoJadwal> getJadwalByPage(Integer id, Integer idHari, Long guruPengajar, Pageable pageable);
     List<dtoJadwal> getJadwalByList();
     Jadwal save(Jadwal jadwal);
     Jadwal delete(Integer id);
