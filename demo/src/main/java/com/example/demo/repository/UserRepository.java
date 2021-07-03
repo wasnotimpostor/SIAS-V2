97891830b63e2e2b8a1888972d2f3be782dffd8e
wasnotimpostor <evan.mahendra95@gmail.com>
2021-07-03 22:46:26 +0700

    repository user

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.example.demo.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
     Users getUserByUsername(String username);
+    Optional<Users> findByUsernameOrEmail(String username, String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
